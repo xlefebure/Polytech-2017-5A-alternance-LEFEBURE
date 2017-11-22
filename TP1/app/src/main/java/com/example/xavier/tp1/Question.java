3a08858a894f584e8d20d1f00255d12866a9a2bb
Epulapp <Epulapp>
2017-11-22 17:46:56 +0100

    Add screens Menu & Question, translation, log in lifecycle and scroll

--- TP1/app/src/main/java/com/example/xavier/tp1/Question.java
+++ TP1/app/src/main/java/com/example/xavier/tp1/Question.java
@@ -1,15 +1,15 @@
 package com.example.xavier.tp1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-public class Menu extends AppCompatActivity {
+public class Question extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_question);
     }
 
     @Override
